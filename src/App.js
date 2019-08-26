5cd7103b0b0f8b5717052e0f5d329e8c0a66d237
Kraken <sufranj.naweed@gmail.com>
2019-08-26 22:43:57 +0200

    base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
       </header>
+
+      <div className="container">
+        <div className="contact-container">
+          <div className="contact-item">
+            <div className="img-container">
+              <img src="" alt="avatar"/>
+            </div>
+
+            <div className="content">
+              <p className="text-center">NAME</p>
+              <p className="">USERNAME</p>
+              <a href="">EMAIL</a>
+              <a href="">Website</a>
+
+              {/* address button to see more */}
+              <p>address</p>
+              <ul>
+                <li>street : </li>
+                <li>suite : </li>
+                <li>zipcode : </li>
+                <li>city : </li>
+                <li>zipcode : </li>
+              </ul>
+
+              {/* button call */}
+              <a href="">TEL</a>
+
+
+              {/* A JSX comment */}
+            </div>
+
+            <div className="deleteButton">
+              X
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
