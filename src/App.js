6c7f8590b2a1a131d3dc1913025e9b1e269343bb
NaweedSufranj <sufranj.naweed@gmail.com>
2019-08-28 12:50:18 +0200

    update: move contact-container & add ContactContainer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ContactContainer from './Component/ContactContainer';
 
 function App() {
   return (
@@ -12,43 +13,8 @@ function App() {
       </header>
 
       <div className="container">
-        <div className="contact-container">
-          <div className="contact-item">
-            <div className="img-container">
-              <img src="" alt="avatar"/>
-            </div>
-
-            <div className="content">
-              <p className="text-center">NAME</p>
-              <p className="">USERNAME</p>
-              <a href="">EMAIL</a>
-              <a href="">Website</a>
-
-              {/* address button to see more */}
-              <p>address</p>
-              <ul>
-                <li>street : </li>
-                <li>suite : </li>
-                <li>zipcode : </li>
-                <li>city : </li>
-                <li>zipcode : </li>
-              </ul>
-
-              {/* button call */}
-              <a href="">TEL</a>
-
-
-              {/* A JSX comment */}
-            </div>
-
-            <div className="deleteButton">
-              X
-            </div>
-          </div>
-        </div>
+        <ContactContainer />
       </div>
-
-
     </div>
   );
 }
