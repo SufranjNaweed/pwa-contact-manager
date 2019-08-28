b8e940c794d98a5ff2cbb38307f5c13a7aa9a411
NaweedSufranj <sufranj.naweed@gmail.com>
2019-08-28 17:45:11 +0200

    add:  header.js

---
+++ src/Component/layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header className="App-header">
+            <ul>
+                <li>Contact</li>
+                <li>About</li>
+                <li>About</li>
+            </ul>
+      </header>
+    )
+}
+
+export default Header;
