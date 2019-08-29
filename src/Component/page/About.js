dd96e8e59edd05ace77ef5b20536dfaee9b69d3f
NaweedSufranj <sufranj.naweed@gmail.com>
2019-08-29 16:00:03 +0200

    update : layout About

--- src/Component/page/About.js
+++ src/Component/page/About.js
@@ -3,30 +3,32 @@ import React from 'react'
 export default function About() {
     return (
         <React.Fragment>
-            <h1>About</h1>
-            <p>
-                This is a student projet made with react and it's a Progressive Web App oriented.
-                <br />
-                Therefor you can install this web app into your mobile phone (Android or Iphone) 
-            </p>
-            <h2>How to install ?</h2>
-            <div className="content-install-instruction">
-                <h3>ANDROID</h3>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt in 
-                    doloremque voluptates ipsa recusandae enim accusamus voluptatibus fugiat itaque commodi! 
-                    Libero, hic enim! Exercitationem consequuntur dignissimos odio impedit enim harum, placeat libero suscipit, 
-                    porro voluptate odit architecto, pariatur maxime ratione?
+            <div id="about" className="container">
+                <h2>About</h2>
+                <p id="presentation">
+                    This is a student projet made with react and it's a Progressive Web App oriented.
+                    <br />
+                    Therefor you can install this web app into your mobile phone (Android or Iphone) 
                 </p>
-            </div>
-            <div className="content-install-instruction">
-                <h3>ANDROID</h3>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt in 
-                    doloremque voluptates ipsa recusandae enim accusamus voluptatibus fugiat itaque commodi! 
-                    Libero, hic enim! Exercitationem consequuntur dignissimos odio impedit enim harum, placeat libero suscipit, 
-                    porro voluptate odit architecto, pariatur maxime ratione?
-                </p>
+                <h2>How to install ?</h2>
+                <div className="content-install-instruction">
+                    <h3>ANDROID</h3>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt in 
+                        doloremque voluptates ipsa recusandae enim accusamus voluptatibus fugiat itaque commodi! 
+                        Libero, hic enim! Exercitationem consequuntur dignissimos odio impedit enim harum, placeat libero suscipit, 
+                        porro voluptate odit architecto, pariatur maxime ratione?
+                    </p>
+                </div>
+                <div className="content-install-instruction">
+                    <h3>IOS</h3>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt in 
+                        doloremque voluptates ipsa recusandae enim accusamus voluptatibus fugiat itaque commodi! 
+                        Libero, hic enim! Exercitationem consequuntur dignissimos odio impedit enim harum, placeat libero suscipit, 
+                        porro voluptate odit architecto, pariatur maxime ratione?
+                    </p>
+                </div>
             </div>
         </React.Fragment>
     )
