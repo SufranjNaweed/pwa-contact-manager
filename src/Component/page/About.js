4eaad90eb9d9f3e03dc8a99296c1ad434b46ad5b
NaweedSufranj <sufranj.naweed@gmail.com>
2019-08-29 14:08:23 +0200

    add :  about page

---
+++ src/Component/page/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>
+                This is a student projet made with react and it's a Progressive Web App oriented.
+                <br />
+                Therefor you can install this web app into your mobile phone (Android or Iphone) 
+            </p>
+            <h2>How to install ?</h2>
+            <div className="content-install-instruction">
+                <h3>ANDROID</h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt in 
+                    doloremque voluptates ipsa recusandae enim accusamus voluptatibus fugiat itaque commodi! 
+                    Libero, hic enim! Exercitationem consequuntur dignissimos odio impedit enim harum, placeat libero suscipit, 
+                    porro voluptate odit architecto, pariatur maxime ratione?
+                </p>
+            </div>
+            <div className="content-install-instruction">
+                <h3>ANDROID</h3>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt in 
+                    doloremque voluptates ipsa recusandae enim accusamus voluptatibus fugiat itaque commodi! 
+                    Libero, hic enim! Exercitationem consequuntur dignissimos odio impedit enim harum, placeat libero suscipit, 
+                    porro voluptate odit architecto, pariatur maxime ratione?
+                </p>
+            </div>
+        </React.Fragment>
+    )
+}
